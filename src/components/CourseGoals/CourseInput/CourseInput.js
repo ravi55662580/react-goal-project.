7d099938452fefb9253dc9e3e62eea7bdd671c82
Your Name <you@example.com>
2024-01-24 16:12:53 +0530

    dynamic styling red button

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,23 +5,28 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
 
   const goalInputChangeHandler = event => {
     setEnteredValue(event.target.value);
+    
+    setIsButtonDisabled(event.target.value.trim() === '');
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
     props.onAddGoal(enteredValue);
+    setEnteredValue('');
+    setIsButtonDisabled(true);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
         <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <input type="text" value = {enteredValue} onChange={goalInputChangeHandler} required/>
       </div>
-      <Button type="submit">Add Goal</Button>
+      <Button type="submit" disabled={isButtonDisabled}>Add Goal</Button>
     </form>
   );
 };
